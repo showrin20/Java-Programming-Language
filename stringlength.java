ca5fab9a530e6bee43f58ee702c2f48725572ff5
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2022-03-27 10:55:11 +0600

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,8 @@
+public class stringlength {
+    public static void main(String[] args){
+        System.out.println("This Program Finds the lenth of a string");
+        String name = "Showrin";
+        System.out.println(name.length());
+    }
+}
+    
